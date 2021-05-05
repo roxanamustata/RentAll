e05557418b62e340f46605baf6ae75a0373294a2
Ralu <roxanamustata@yahoo.com>
2021-05-05 15:32:59 +0300

    Share connection and transaction assignment

--- RentAll/RentAll.ConsoleApp/Program.cs
+++ RentAll/RentAll.ConsoleApp/Program.cs
@@ -56,83 +56,97 @@ namespace RentAll.ConsoleApp
             //Share connection and transaction (multiple context instances that share the same connection
             // UseTransaction to enlist both contexts in the same transaction
 
-            var options = new DbContextOptionsBuilder<RentAllDbContext>()
-                .UseSqlServer(new SqlConnection(connectionString)).Options;
+            //var options = new DbContextOptionsBuilder<RentAllDbContext>()
+            //    .UseSqlServer(new SqlConnection(connectionString)).Options;
 
-            using (var context1 = new RentAllDbContext(options))
-            {
-                using (var transaction = context1.Database.BeginTransaction())
-                {
-                    try
-                    {
-                        context1.Categories.Add(new Category { CategoryName = "Events" });
-                        context1.SaveChanges();
+            //using (var context1 = new RentAllDbContext(options))
+            //{
+            //    using (var transaction = context1.Database.BeginTransaction())
+            //    {
+            //        try
+            //        {
+            //            context1.Categories.Add(new Category { CategoryName = "Health" });
+            //            context1.SaveChanges();
 
-                        using (var context2 = new RentAllDbContext(options))
-                        {
-                            context2.Database.UseTransaction(transaction.GetDbTransaction());
-                            var categories = context2.Categories.OrderBy(c => c.CategoryName).ToList();
-                            //context2.Categories.Add(new Category { CategoryName = "Education" });
-                            //context2.SaveChanges();
-                        }
-                        transaction.Commit();
-                    }
-                    catch (Exception)
-                    {
-                        //transaction.Rollback();
-                    }
+            //            using (var context2 = new RentAllDbContext(options))
+            //            {
+            //                context2.Database.UseTransaction(transaction.GetDbTransaction());
+            //                var categories = context2.Categories.OrderBy(c => c.CategoryName).ToList();
+            //                foreach (var item in categories)
+            //                {
+            //                    Console.WriteLine(item.CategoryName);
+            //                }
 
 
+            //            }
+            //            transaction.Commit();
+            //        }
+            //        catch (Exception)
+            //        {
+            //            //transaction.Rollback();
+            //        }
 
 
-                }
 
 
-                //}
-                //using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                //        new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted }))
-                //{
-                //    using (var connection = new SqlConnection(connectionString))
-                //    {
-                //        connection.Open();
-
-                //        using (var transaction = connection.BeginTransaction())
-                //        {
-                //            try
-                //            {
-                //                //Run Ado.Net command in the transaction
-                //                var command = connection.CreateCommand();
-                //                command.Transaction = transaction;
-                //                command.CommandText = "Delete FROM dbo.Categories";
-                //                command.ExecuteNonQuery();
-
-                //                //Run EF Core command in the transaction
-                //                //var options2 = new DbContextOptionsBuilder<RentAllDbContext>()
-                //                //    .UseSqlServer(connection).Options;
-
-                //                //using (var context = new RentAllDbContext(options2))
-                //                //{
-                //                //    context.Database.UseTransaction(transaction);
-                //                //    context.Categories.Add(new Category { CategoryName = "Health" });
-                //                //    context.SaveChanges();
-
-                //                //}
-                //                transaction.Commit();
-                //            }
-                //            catch (System.Exception)
-                //            {
-                //                //transaction.Rollback();
-                //            }
-                //        }
-
-                //    }
-                //}
+            //    }
 
 
-            }
+
+            //using (var scope = new TransactionScope(TransactionScopeOption.Required,
+            //        new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted }))
+            //{
+            //using (var connection = new SqlConnection(connectionString))
+            //{
+            //    connection.Open();
+
+            //    using (var transaction = connection.BeginTransaction())
+            //    {
+            //    try
+            //    {
+            //        //Run Ado.Net command in the transaction
+            //        var command = connection.CreateCommand();
+            //            command.Transaction = transaction;
+            //            command.CommandText = "INSERT INTO dbo.Categories VALUES('Park')";
+            //            command.ExecuteNonQuery();
+
+            //            //Run EF Core command in the transaction
+            //            var options2 = new DbContextOptionsBuilder<RentAllDbContext>()
+            //                .UseSqlServer(connection).Options;
+
+            //            using (var context = new RentAllDbContext(options2))
+            //            {
+            //                context.Database.UseTransaction(transaction);
+            //                context.Categories.Add(new Category { CategoryName = "Health" });
+            //                context.SaveChanges();
+
+            //            }
+            //            transaction.Commit();
+            //    }
+            //    catch (System.Exception)
+            //    {
+            //        //transaction.Rollback();
+            //    }
+            //}
+
+            //}
+            //}
+
+
+            //    create queries, use filters, projections and ordering.
+            //    investigate what queries are generated in sql profile
+
+
+
+
+
+          //  Create queries with all types of joins.
+
+
         }
     }
-}
+    }
+
 
 
 
--- RentAll/RentAll.Infrastructure/Data/RentAllDbContext.cs
+++ RentAll/RentAll.Infrastructure/Data/RentAllDbContext.cs
@@ -12,23 +12,23 @@ namespace RentAll.Infrastructure.Data
 {
     public class RentAllDbContext : DbContext
     {
-        private readonly string _connectionString = "Data Source=RALU\\SQLEXPRESS;Initial Catalog=RentAllDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
-            "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //private readonly string _connectionString = "Data Source=RALU\\SQLEXPRESS;Initial Catalog=RentAllDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
+        //    "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-        public RentAllDbContext() : base()
-        {
+        //public RentAllDbContext() : base()
+        //{
 
-        }
+        //}
 
         public RentAllDbContext(DbContextOptions<RentAllDbContext> options) : base(options)
         {
 
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(_connectionString);
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(_connectionString);
+        //}
 
         public DbSet<Activity> Activities { get; set; }
         public DbSet<Category> Categories { get; set; }
