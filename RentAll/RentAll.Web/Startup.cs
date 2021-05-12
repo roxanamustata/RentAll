6d67a3d7e606f391aff2cf2b00783564b447c03c
Ralu <roxanamustata@yahoo.com>
2021-05-12 14:42:05 +0300

    Add Swagger to web project

--- RentAll/RentAll.Web/Startup.cs
+++ RentAll/RentAll.Web/Startup.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace RentAll.Web
 {
@@ -26,6 +26,13 @@ namespace RentAll.Web
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RentAllWeb", Version = "v1" });
+            });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +41,8 @@ namespace RentAll.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RentAllWeb v1"));
             }
             else
             {
